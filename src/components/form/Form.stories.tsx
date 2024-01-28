634dc29eaf3ffc2a9ef25ad1cf9c9db2e9d41c49
Masashi Kondo <masashi.kondo@rokka.dev>
2024-01-28 00:25:50 -0800

    fix form

--- src/components/form/Form.stories.tsx
+++ src/components/form/Form.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Labeled } from "../labeled/Labeled.tsx";
 import { Form } from "./Form.tsx";
+import {Button} from "../button/Button.tsx";
 
 const meta = {
 	title: "Components/Forms/Form",
@@ -18,6 +19,20 @@ export const Default: Story = {
 		<Form>
 			<Labeled.TextField label="label 1" />
 			<Labeled.TextField label="label 2" />
+			<Form.Footer><Button.Submit label="Submit"/></Form.Footer>
+		</Form>
+	),
+	args: {
+		children: undefined,
+	},
+};
+
+export const Horizontal: Story = {
+	render: (_) => (
+		<Form horizontal>
+			<Labeled.TextField label="label 1" />
+			<Labeled.TextField label="label 2" />
+			<Form.Footer><Button.Submit label="Submit"/></Form.Footer>
 		</Form>
 	),
 	args: {
--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -2,9 +2,12 @@ import { ReactNode } from "react";
 import "./form.css";
 
 type Props = {
+	horizontal?: boolean;
 	children: ReactNode;
 };
 
-const Form = ({ children }: Props) => <form>{children}</form>;
+const Form = ({ horizontal = undefined, children }: Props) => <form data-horizontal={horizontal}>{children}</form>;
+
+Form.Footer = ({ children }: Props) => <footer>{children}</footer>;
 
 export { Form };
