94f7db96b3a39c9b9c081be17f7964429d8676f6
Masashi Kondo <masashi.kondo@rokka.dev>
2024-01-30 16:08:06 -0800

    bun format

--- src/components/icon/Icon.stories.tsx
+++ src/components/icon/Icon.stories.tsx
@@ -1,24 +1,24 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import {Icon} from "./Icon.tsx";
+import { Icon } from "./Icon.tsx";
 
 const meta = {
-  title: "Components/Icon",
-  component: Icon,
-  parameters: {
-    layout: "centered",
-  },
-  tags: ["autodocs"],
-  args: {
-    size: "m",
-  },
-  argTypes: {
-    size: {
-      control: {
-        type: "radio",
-      },
-      options: ["s", "m", "l"],
-    }
-  }
+	title: "Components/Icon",
+	component: Icon,
+	parameters: {
+		layout: "centered",
+	},
+	tags: ["autodocs"],
+	args: {
+		size: "m",
+	},
+	argTypes: {
+		size: {
+			control: {
+				type: "radio",
+			},
+			options: ["s", "m", "l"],
+		},
+	},
 } satisfies Meta<typeof Icon>;
 
 export default meta;
@@ -41,13 +41,15 @@ Icon.Error.displayName = "Icon.Error";
 Icon.Calender.displayName = "Icon.Calender";
 
 export const Default: Story = {
-  render: (args) => <>
-    <Icon.Github {...args}/>
-    <Icon.Trash {...args}/>
-    <Icon.Check {...args}/>
-    <Icon.Info {...args}/>
-    <Icon.Warning {...args}/>
-    <Icon.Error {...args}/>
-    <Icon.Calender {...args}/>
-  </>,
+	render: (args) => (
+		<>
+			<Icon.Github {...args} />
+			<Icon.Trash {...args} />
+			<Icon.Check {...args} />
+			<Icon.Info {...args} />
+			<Icon.Warning {...args} />
+			<Icon.Error {...args} />
+			<Icon.Calender {...args} />
+		</>
+	),
 };
--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,37 +1,69 @@
-import { IconContext } from 'react-icons'
+import { ReactNode } from "react";
+import { IconContext } from "react-icons";
 import {
-  FiAlertCircle,
-  FiAlertTriangle,
-  FiCalendar,
-  FiCheckCircle,
-  FiGithub,
-  FiTrash,
-  FiXCircle
-} from "react-icons/fi"
-import {ReactNode} from "react";
+	FiAlertCircle,
+	FiAlertTriangle,
+	FiCalendar,
+	FiCheckCircle,
+	FiGithub,
+	FiTrash,
+	FiXCircle,
+} from "react-icons/fi";
 
 type Props = {
-  size?: "s" | "m" | "l"
-}
+	size?: "s" | "m" | "l";
+};
 
 type BaseProps = {
-  children: ReactNode
-} & Props
+	children: ReactNode;
+} & Props;
 
-const Icon = ({size = 'm', children }: BaseProps) => <IconContext.Provider value={{style: {fontSize: sizeMap[size]}}}>{children}</IconContext.Provider>
+const Icon = ({ size = "m", children }: BaseProps) => (
+	<IconContext.Provider value={{ style: { fontSize: sizeMap[size] } }}>
+		{children}
+	</IconContext.Provider>
+);
 
 const sizeMap = {
-  s: "10px",
-  m: "16px",
-  l: "24px",
-} as const
+	s: "10px",
+	m: "16px",
+	l: "24px",
+} as const;
 
-Icon.Github = (props: Props) => <Icon {...props}><FiGithub/></Icon>
-Icon.Check = (props: Props) => <Icon {...props}><FiCheckCircle/></Icon>
-Icon.Trash = (props: Props) => <Icon {...props}><FiTrash/></Icon>
-Icon.Info = (props: Props) => <Icon {...props}><FiAlertCircle/></Icon>
-Icon.Warning = (props: Props) => <Icon {...props}><FiAlertTriangle/></Icon>
-Icon.Error = (props: Props) => <Icon {...props}><FiXCircle/></Icon>
-Icon.Calender = (props: Props) => <Icon {...props}><FiCalendar/></Icon>
+Icon.Github = (props: Props) => (
+	<Icon {...props}>
+		<FiGithub />
+	</Icon>
+);
+Icon.Check = (props: Props) => (
+	<Icon {...props}>
+		<FiCheckCircle />
+	</Icon>
+);
+Icon.Trash = (props: Props) => (
+	<Icon {...props}>
+		<FiTrash />
+	</Icon>
+);
+Icon.Info = (props: Props) => (
+	<Icon {...props}>
+		<FiAlertCircle />
+	</Icon>
+);
+Icon.Warning = (props: Props) => (
+	<Icon {...props}>
+		<FiAlertTriangle />
+	</Icon>
+);
+Icon.Error = (props: Props) => (
+	<Icon {...props}>
+		<FiXCircle />
+	</Icon>
+);
+Icon.Calender = (props: Props) => (
+	<Icon {...props}>
+		<FiCalendar />
+	</Icon>
+);
 
-export { Icon }
+export { Icon };
