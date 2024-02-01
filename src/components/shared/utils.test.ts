5b61e256eccb55c3c756047c9731bebcf935d44c
Masashi Kondo <masashi.kondo@rokka.dev>
2024-01-31 22:46:17 -0800

    add classNames

--- src/components/shared/utils.test.ts
+++ src/components/shared/utils.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "vitest";
-import { isEmpty } from "./utils.ts";
+import { classNames, isEmpty, isTrue, isUndefined } from "./utils.ts";
 
 describe("isEmpty", () => {
 	test("returns true for empty object", () => {
@@ -8,5 +8,42 @@ describe("isEmpty", () => {
 
 	test("returns false for non-empty object", () => {
 		expect(isEmpty({ a: 1 })).toBe(false);
+	});
+});
+
+describe("isTrue", () => {
+	test("returns true for true", () => {
+		expect(isTrue(true)).toBe(true);
+	});
+
+	test("It returns false if the value is anything other than true.", () => {
+		expect(isTrue(false)).toBe(false);
+		expect(isTrue(1)).toBe(false);
+		expect(isTrue("true")).toBe(false);
+		expect(isTrue(undefined)).toBe(false);
+		expect(isTrue(null)).toBe(false);
+		expect(isTrue({})).toBe(false);
+	});
+});
+
+describe("isUndefined", () => {
+	test("returns true for undefined", () => {
+		expect(isUndefined(undefined)).toBe(true);
+	});
+
+	test("returns false for non-undefined", () => {
+		expect(isUndefined(true)).toBe(false);
+		expect(isUndefined(1)).toBe(false);
+		expect(isUndefined("undefined")).toBe(false);
+		expect(isUndefined(null)).toBe(false);
+		expect(isUndefined({})).toBe(false);
+	});
+});
+
+describe("classNames", () => {
+	test("returns a string of class names", () => {
+		expect(classNames({ a: 1, b: "2", c: undefined, d: true })).toBe(
+			"a:1 b:2 d",
+		);
 	});
 });
--- src/components/shared/utils.ts
+++ src/components/shared/utils.ts
@@ -1,3 +1,20 @@
 const isEmpty = (obj: object) => Object.keys(obj).length === 0;
+const isTrue = (value: unknown): value is true =>
+	typeof value === "boolean" && value;
 
-export { isEmpty };
+const isUndefined = (value: unknown): value is undefined =>
+	typeof value === "undefined";
+
+type PropType = string | number | boolean | undefined;
+type Prop = Record<string, PropType>;
+
+const classNames = (props: Prop) =>
+	Object.entries(props).flatMap(convertClassName).join(" ");
+
+const convertClassName = ([name, value]: [string, PropType]) => {
+	if (isUndefined(value)) return [];
+	if (isTrue(value)) return [name];
+	return [`${name}:${value}`];
+};
+
+export { isEmpty, isTrue, isUndefined, classNames };
