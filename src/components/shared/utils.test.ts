d7282ae90c3e6b9dc0d141e70ee0468d52432824
Masashi Kondo <masashi.kondo@rokka.dev>
2024-02-01 00:14:24 -0800

    add test

--- src/components/shared/utils.test.ts
+++ src/components/shared/utils.test.ts
@@ -1,5 +1,12 @@
 import { describe, expect, test } from "vitest";
-import { classNames, isEmpty, isTrue, isUndefined } from "./utils.ts";
+import {
+	camel2KebabCase,
+	cn,
+	isEmpty,
+	isFalsyExcludingZero,
+	isTrue,
+	isUndefined,
+} from "./utils.ts";
 
 describe("isEmpty", () => {
 	test("returns true for empty object", () => {
@@ -13,10 +20,9 @@ describe("isEmpty", () => {
 
 describe("isTrue", () => {
 	test("returns true for true", () => {
+		// true
 		expect(isTrue(true)).toBe(true);
-	});
-
-	test("It returns false if the value is anything other than true.", () => {
+		// false
 		expect(isTrue(false)).toBe(false);
 		expect(isTrue(1)).toBe(false);
 		expect(isTrue("true")).toBe(false);
@@ -28,10 +34,9 @@ describe("isTrue", () => {
 
 describe("isUndefined", () => {
 	test("returns true for undefined", () => {
+		// true
 		expect(isUndefined(undefined)).toBe(true);
-	});
-
-	test("returns false for non-undefined", () => {
+		// false
 		expect(isUndefined(true)).toBe(false);
 		expect(isUndefined(1)).toBe(false);
 		expect(isUndefined("undefined")).toBe(false);
@@ -40,10 +45,44 @@ describe("isUndefined", () => {
 	});
 });
 
+describe("isFalsyExcludingZero", () => {
+	test("returns true for false, null, undefined, empty string, and NaN", () => {
+		// true
+		expect(isFalsyExcludingZero(false)).toBe(true);
+		expect(isFalsyExcludingZero(null)).toBe(true);
+		expect(isFalsyExcludingZero(undefined)).toBe(true);
+		expect(isFalsyExcludingZero("")).toBe(true);
+		expect(isFalsyExcludingZero(Number.NaN)).toBe(true);
+		// false
+		expect(isFalsyExcludingZero(true)).toBe(false);
+		expect(isFalsyExcludingZero(0)).toBe(false);
+		expect(isFalsyExcludingZero("false")).toBe(false);
+	});
+});
+
+describe("camel2KebabCase", () => {
+	test("converts camelCase to kebab-case", () => {
+		expect(camel2KebabCase("camelCase")).toBe("camel-case");
+		expect(camel2KebabCase("superCamelCase")).toBe("super-camel-case");
+		expect(camel2KebabCase("camelCase1")).toBe("camel-case-1");
+	});
+});
+
 describe("classNames", () => {
 	test("returns a string of class names", () => {
-		expect(classNames({ a: 1, b: "2", c: undefined, d: true })).toBe(
-			"a:1 b:2 d",
-		);
+		expect(
+			cn("default_1 default_2", {
+				a: 1,
+				b: "2",
+				c: undefined,
+				d: true,
+				e: 0,
+				camelCase1: true,
+				camelCase2: 0,
+			}),
+		).toBe("default_1 default_2 a_1 b_2 d e_0 camel-case-1 camel-case-2_0");
+	});
+	test("returns a string of class names without duplication", () => {
+		expect(cn("default_1 default_1", { default: 1 })).toBe("default_1");
 	});
 });
--- src/components/shared/utils.ts
+++ src/components/shared/utils.ts
@@ -5,16 +5,43 @@ const isTrue = (value: unknown): value is true =>
 const isUndefined = (value: unknown): value is undefined =>
 	typeof value === "undefined";
 
+const isFalsyExcludingZero = (value: unknown): boolean => {
+	return (
+		value === false ||
+		value === null ||
+		isUndefined(value) ||
+		value === "" ||
+		Number.isNaN(value)
+	);
+};
+
+const camel2KebabCase = (str: string) =>
+	str.replace(/([a-z])([A-Z0-9])/g, "$1-$2").toLowerCase();
+
 type PropType = string | number | boolean | undefined;
 type Prop = Record<string, PropType>;
 
-const classNames = (props: Prop) =>
-	Object.entries(props).flatMap(convertClassName).join(" ");
-
+/**
+ * Convert props to class names without duplicating
+ * @param defaultClassName expected space separated class names ex: "text red bg-white"
+ * @param props object of props ex: { text: true, bg: "white" }
+ **/
+const cn = (defaultClassName = "", props: Prop) => {
+	const defaultClassNames = defaultClassName.split(" ");
+	const classNames = Object.entries(props).flatMap(convertClassName);
+	return Array.from(new Set([...defaultClassNames, ...classNames])).join(" ");
+};
 const convertClassName = ([name, value]: [string, PropType]) => {
-	if (isUndefined(value)) return [];
-	if (isTrue(value)) return [name];
-	return [`${name}:${value}`];
+	if (isFalsyExcludingZero(value)) return [];
+	if (isTrue(value)) return [camel2KebabCase(name)];
+	return [`${camel2KebabCase(name)}_${value}`];
 };
 
-export { isEmpty, isTrue, isUndefined, classNames };
+export {
+	isEmpty,
+	isTrue,
+	isUndefined,
+	isFalsyExcludingZero,
+	camel2KebabCase,
+	cn,
+};
